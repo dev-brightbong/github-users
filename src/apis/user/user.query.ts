fa340355de203b1226bec222fc3e612daa662d16
장현봉 <wkdgusqhd080@naver.com>
2024-05-26 19:25:35 +0900

    chore: query-key naming 수정

--- src/apis/user/user.query.ts
+++ src/apis/user/user.query.ts
@@ -2,7 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { InfiniteQueryHookParams, Parameter } from "../react-query.type";
 import { userApi } from "./user.api";
 
-export const USERS_API_QUERY_KEY = {
+export const USER_API_QUERY_KEY = {
   SEARCH_USERS_INFINITE: (params?: Parameter<typeof userApi.searchUsers>) =>
     ["SEARCH_USERS_INFINITE", params].filter(
       (key) => typeof key !== "undefined"
@@ -12,7 +12,7 @@ export const USERS_API_QUERY_KEY = {
 export const useSearchUsersInfiniteQuery = (
   params: InfiniteQueryHookParams<typeof userApi.searchUsers, any>
 ) => {
-  const queryKey = USERS_API_QUERY_KEY.SEARCH_USERS_INFINITE(params.variables);
+  const queryKey = USER_API_QUERY_KEY.SEARCH_USERS_INFINITE(params.variables);
   return useInfiniteQuery(
     queryKey,
     ({ pageParam }) => {
