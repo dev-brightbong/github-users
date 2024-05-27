ecd940f5d5a02348804f8d5e6737cee6bc260e28
장현봉 <wkdgusqhd080@naver.com>
2024-05-27 23:16:38 +0900

    feat: 검색인풋 Enter 이벤트 추가

--- src/containers/Users/SearchInput.tsx
+++ src/containers/Users/SearchInput.tsx
@@ -1,5 +1,11 @@
 import React, { memo, useState } from "react";
-import { Button, Input, InputGroup, InputRightAddon } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  InputGroup,
+  InputRightAddon,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 const SearchInput = () => {
@@ -11,24 +17,27 @@ const SearchInput = () => {
     setInput(value);
   };
 
-  const handleSearch = () => {
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (input === "") return;
     router.push({ pathname: "/users", query: { query: input } }, "", {
       shallow: true,
     });
   };
   return (
-    <InputGroup>
-      <Input
-        mb="24px"
-        placeholder="유저를 검색해보세요."
-        value={input}
-        onChange={onChangeInput}
-      />
-      <InputRightAddon>
-        <Button onClick={handleSearch}>검색</Button>
-      </InputRightAddon>
-    </InputGroup>
+    <Box as="form" onSubmit={handleSearch}>
+      <InputGroup>
+        <Input
+          mb="24px"
+          placeholder="유저를 검색해보세요."
+          value={input}
+          onChange={onChangeInput}
+        />
+        <InputRightAddon>
+          <Button type="submit">검색</Button>
+        </InputRightAddon>
+      </InputGroup>
+    </Box>
   );
 };
 
